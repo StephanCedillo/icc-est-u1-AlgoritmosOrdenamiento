116f225f4f891217666032e32196268b59c68ce4
Stephan Cedillo <stephan.cedillo@gmail.com>
2026-04-29 13:04:06 -0500

    FIX: Insertion print

--- src/App.java
+++ src/App.java
@@ -6,16 +6,17 @@ public class App {
         Scanner leer = new Scanner(System.in);
         InsertionSort insercion = new InsertionSort();
 
-        int[] array = { 0, 1, 4, 5, 2, 5, 6, 7, 8, 1, 23, 14 };
+        
         System.out.println("------Menu------\n 1.Ejecutar Ordenamientos\n 2.Salir");
         int opcionMenu = leer.nextInt();
         int[] valoresObtenidos;
         while (opcionMenu != 2) {
+            int[] array = { 0, 1, 4, 5, 2, 5, 6, 7, 8, 1, 23, 14 };
             if (opcionMenu == 1) {
                 System.out.println("Metodo de Insercion");
                 System.out.println("Usted quiere ordenar ascendente? (S/N)");
                 String ordenamiento = leer.next();
-                while (ordenamiento.equalsIgnoreCase("S") && ordenamiento.equalsIgnoreCase("N")) {
+                while (!ordenamiento.equalsIgnoreCase("S") && !ordenamiento.equalsIgnoreCase("N")) {
                     System.out.println("Usted ingreso un valor no detectado, vuelva a ingresar");
                     System.out.println("Usted quiere ordenar ascendente? (S/N)");
                     ordenamiento = leer.next();
@@ -31,6 +32,8 @@ public class App {
             } else {
                 System.out.println("Usted ingreso la entrada mal ");
             }
+            System.out.println("------Menu------\n 1.Ejecutar Ordenamientos\n 2.Salir");
+            opcionMenu = leer.nextInt();
         }
         System.out.println("Usted salio del programa");
 
--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 
 public class InsertionSort {
     InsertionSort() {
@@ -7,41 +6,60 @@ public class InsertionSort {
     int aux;
 
     public int[] sort(int[] arreglo, boolean asc) {
-        int[] array = arreglo;
+        int[] array = arreglo.clone();
         int[] valoresObtenidos={
             0//Comparaciones
             ,0//Cambios
             ,0//Iteraciones
         };
         
-        System.out.println("Arreglo Original" + Arrays.toString(array));
+        System.out.println("Arreglo Original" );
+        printArray(array);
         for (int i = 0; i < array.length; i++) {
             aux = array[i];
             int j = i;
-            System.out.println("Iteracion: " + (i+1));
+            System.out.println("Iteracion: " + (i+1) + "\n");
             System.out.println("Cambios del Proceso: ");
             if (asc) {
+                // ascendente
                 while (j > 0 && array[j - 1] > aux) {
                     array[j] = array[j - 1];
                     j--;
                     valoresObtenidos[1]++;
-                    System.out.println("El elemento "+ array[j - 1] + " cambia por el "+ array[j] +" quedando : "+Arrays.toString(array));
+                    System.out.println(" \n Cambio dentro de la iteracion: ");
+                    printArray(array);
+                    valoresObtenidos[0]++;
+                
                 
                 }
+                valoresObtenidos[0]++;
+                
             } else {
+                // descendente 
                 while (j > 0 && array[j - 1] < aux) {
                     array[j] = array[j - 1];
                     j--;
                     valoresObtenidos[1]++;
-                    System.out.println("El elemento "+ array[j - 1] + " cambia por el "+ array[j] +" quedando : "+Arrays.toString(array));
-
+                    System.out.println(" \n Cambio dentro de la iteracion: ");
+                    printArray(array);
+                    valoresObtenidos[0]++;
                 }
+                valoresObtenidos[0]++;
+
             }
 
             array[j] = aux;
             valoresObtenidos[2]++;
         }
-        System.out.println("Arreglo Ordenado" + Arrays.toString(array));
-        return array;
+        System.out.println("Arreglo Ordenado");
+        printArray(array);
+        return valoresObtenidos;
+    }
+    public void printArray(int[] array){
+        System.out.print(" Arreglo : [");
+        for (int i : array) {
+            System.out.print(i + " , ");
+        } 
+        System.out.println("]");
     }
 }
